2b521a176eda6b995847672f0b83953384df6ef3
maverick <maverickzhang2018@gmail.com>
2020-02-22 22:34:58 +0800

    v0.1

---
+++ include/XFTPLIST.h
@@ -0,0 +1,14 @@
+//
+// Created by maverick on 2020/2/22.
+//
+
+#ifndef FTP_SERVER_XFTPLIST_H
+#define FTP_SERVER_XFTPLIST_H
+
+
+class XFTPLIST {
+
+};
+
+
+#endif //FTP_SERVER_XFTPLIST_H
---
+++ src/XFTPLIST.cpp
@@ -0,0 +1,5 @@
+//
+// Created by maverick on 2020/2/22.
+//
+
+#include "XFTPLIST.h"
